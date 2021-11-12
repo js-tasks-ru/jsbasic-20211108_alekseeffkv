5942e351bb41c7366382fda5976d44a79d6bf0c7
Konstantin Alekseev <alekseeffkv@gmail.com>
2021-11-12 13:06:18 +0300

    tasks of the 2nd module

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  if ( (n ^ 0) !== n || n < 0) {
+    return 'n должно быть целое неотрицательное число';
+  } else if (n == 0) {
+    return 1;
+  } else {
+    let factorialN = 1;
+    for (let i = 0; i < n; i++) {
+      factorialN *= n - i;
+    }
+    return factorialN;
+  }
 }
