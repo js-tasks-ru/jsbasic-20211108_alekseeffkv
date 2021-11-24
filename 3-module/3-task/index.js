d949abeeeb281e5a32b0adf8c96f0e351b59baec
Konstantin Alekseev <alekseeffkv@gmail.com>
2021-11-24 09:25:50 +0300

    tasks of the 3rd module corrected

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,13 +1,5 @@
 function camelize(str) {
   let arrFromStr = str.split('-');
-  if (arrFromStr[0] == '') {
-    arrFromStr.shift();
-    return arrFromStr.map(a => a[0].toUpperCase() + a.slice(1))
-                     .join('');
-  } else {
-    let newStr = arrFromStr.map(a => a[0].toUpperCase() + a.slice(1))
-                         .join('');
-    return newStr[0].toLowerCase() + newStr.slice(1);
-  }
+  return arrFromStr.map( (a, b) => b == 0 ? a: a[0].toUpperCase() + a.slice(1))
+                   .join('');
 }
-
