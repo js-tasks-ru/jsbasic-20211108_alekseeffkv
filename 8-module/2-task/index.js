6fca9c08ff6ee2797269b6b87a2dcb07b06dc5a1
Konstantin Alekseev <alekseeffkv@gmail.com>
2021-12-14 15:17:01 +0300

    tasks of the 8th module

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -2,8 +2,63 @@ import createElement from '../../assets/lib/create-element.js';
 import ProductCard from '../../6-module/2-task/index.js';
 
 export default class ProductGrid {
+  products = null;
+  elem = null;
+
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.elem = this.#createProductGrid();
+  }
+
+  #createCards(products) {
+    return products.map(product => {
+            let productCard = new ProductCard(product);
+            return productCard.elem.outerHTML;
+          }).join('')
+  }
+
+  #createProductGrid() {
+    let productGrid = createElement(`
+    <div class="products-grid">
+      <div class="products-grid__inner">
+        ${this.#createCards(this.products)}
+      </div>
+    </div>
+    `);
+
+    return productGrid;
+  }
+
+  #mergeFilters(newFilter) {
+    for (let item in newFilter) {
+      if (this.filters[item] === undefined || this.filters[item] != newFilter[item]) {
+        this.filters[item] = newFilter[item];
+      }
+    }
+  }
+
+  updateFilter(filters) {
+    let filteredCards = this.products;
+
+    this.#mergeFilters(filters);
+    
+    if (this.filters.noNuts) {
+      filteredCards = filteredCards.filter(product => product.nuts == false || product.nuts == undefined);
+    }
+
+    if (this.filters.vegeterianOnly) {
+      filteredCards = filteredCards.filter(product => product.vegeterian == true);
+    }
+
+    if (this.filters.maxSpiciness) {
+      filteredCards = filteredCards.filter(product => product.spiciness <= this.filters.maxSpiciness);
+    }
+
+    if ( !(this.filters.category == '' || this.filters.category == undefined) ) {
+      filteredCards = filteredCards.filter(product => product.category == this.filters.category);
+    }
+
+    document.querySelector('.products-grid__inner').innerHTML = this.#createCards(filteredCards);
   }
 }
--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,49 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (product === null || product === undefined) return;
+
+    let itemIndex = this.cartItems.findIndex(item => item.product.id == product.id);
+    
+    if (itemIndex == -1) {
+      this.cartItems.push({product, count: 1});
+    } else {
+      this.cartItems[itemIndex].count += 1;
+    }
+
+    this.onProductUpdate(this.cartItems[itemIndex]);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let itemIndex = this.cartItems.findIndex(item => item.product.id == productId);
+    
+    if (amount == 1) {
+      this.cartItems[itemIndex].count += 1;
+    } else if (amount == -1) {
+      this.cartItems[itemIndex].count -= 1;
+    }
+
+    if (this.cartItems[itemIndex].count == 0) {
+      this.cartItems.splice(itemIndex, 1);
+    }
+
+    this.onProductUpdate(this.cartItems[itemIndex]);
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length == 0 ? true : false;
   }
 
   getTotalCount() {
-    // ваш код
+    let totalCount = 0;
+    this.cartItems.map(item => totalCount += item.count);
+    return totalCount;
   }
 
   getTotalPrice() {
-    // ваш код
+    let totalPrice = 0;
+    this.cartItems.map(item => totalPrice += item.count * item.product.price);
+    return totalPrice;
   }
 
   onProductUpdate(cartItem) {
